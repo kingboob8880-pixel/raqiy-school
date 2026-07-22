2b95a5e5e5c16ce9a16bf2401987b4c9b33bd9da
Абу Мухаммад <kingboob8880@gmail.com>
2026-07-22 08:20:03 +0500

    config: Telegram bot token + chat ID

--- integration/telegram-config.js
+++ integration/telegram-config.js
@@ -1,4 +1,4 @@
 // Конфигурация Telegram-бота для уведомлений наставнику.
 // Замени BOT_TOKEN и CHAT_ID на реальные значения из @BotFather / getUpdates.
-export const TG_BOT_TOKEN = "ВСТАВЬ_ТОКЕН_СЮДА";
-export const TG_CHAT_ID   = "ВСТАВЬ_CHAT_ID_СЮДА";
+export const TG_BOT_TOKEN = "8996853653:AAH5wykwBERZfdcMA86gJ5SfERpHEXKmqSA";
+export const TG_CHAT_ID   = "200039060";
